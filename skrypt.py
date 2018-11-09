bab4b8f4284bf16e3991d920e967534567af344a
piotr <piotr@macbook>
2018-11-09 21:59:32 +0100

    #1

--- skrypt.py
+++ skrypt.py
@@ -1,3 +1,6 @@
+# importowanie klasy Pracownik z modulu pracownik (warunek - modul pracownik snajduje sie w ym samym katalgu co ten skrypt
+# from pracownik import Pracownik
+
 # importowanie klasy Pracownik z modulu pracownik znajdujacego sie w pakiecie lab6
 from lab6.pracownik import Pracownik
 
